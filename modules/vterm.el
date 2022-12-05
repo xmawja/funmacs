08d52f47a5c8ea019fab69dd0fa91175b9765ecf
Muja Mo <muja@foss.land>
2022-12-05 09:39:36 +0100

    ADDED: vterm-toggle.el toggles between the vterm buffer
    This package provides the command vterm-toggle which toggles between the vterm buffer and whatever buffer you are editing.

--- modules/vterm.el
+++ modules/vterm.el
@@ -2,4 +2,34 @@
 ;; Enable vterm.el
 (use-package vterm
   :straight t
-  )
+  :init
+  (setq vterm-toggle-fullscreen-p nil)
+  (add-to-list 'display-buffer-alist
+			   '((lambda (buffer-or-name _)
+                   (let ((buffer (get-buffer buffer-or-name)))
+                     (with-current-buffer buffer
+                       (or (equal major-mode 'vterm-mode)
+                           (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
+                (display-buffer-reuse-window display-buffer-at-bottom)
+                ;;(display-buffer-reuse-window display-buffer-in-direction)
+                ;;display-buffer-in-direction/direction/dedicated is added in emacs27
+                ;;(direction . bottom)
+                ;;(dedicated . t) ;dedicated is supported in emacs27
+                (reusable-frames . visible)
+                (window-height . 0.3))))
+
+;; Enable vterm-toggle.el
+(use-package vterm-toggle
+  :bind
+  (("C-x v t"        . vterm-toggle)
+   :map vterm-mode-map
+   ("<C-return>" . vterm-toggle-insert-cd)
+   ("s-n" . vterm-toggle-forward)
+   ("s-p" . vterm-toggle-backward))
+  :config
+  (add-to-list 'display-buffer-alist
+     '("\*vterm\*"
+       (display-buffer-in-side-window)
+       (window-height . 0.3)
+       (side . bottom)
+       (slot . 0))))
