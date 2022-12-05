e5f4c68c886d6c8bed96a90537fbb45028a0c5ee
Muja Mo <muja@foss.land>
2022-12-05 06:31:12 +0100

    ADDED: lsp.el client/library for the Language Server Protocol
    Client for Language Server Protocol (v3.14). lsp-mode aims to provide IDE-like experience by providing optional integration with the most popular Emacs packages like company, flycheck and projectile.

--- init.el
+++ init.el
@@ -1,15 +1,23 @@
 ;;=============== IMPORTS ===============;;
+;; import directorys
 (add-to-list 'load-path "~/.emacs.d/core/")
 (add-to-list 'load-path "~/.emacs.d/modules")
+(add-to-list 'load-path "~/.emacs.d/modules/lsp")
 
+;; core elisp 
 (load-library "settings")
 (load-library "straight")
 (load-library "keymaps")
 
+;; modules elisp
 (load-library "vertico")
 (load-library "magit")
 (load-library "ui")
 (load-library "treemacs")
 (load-library "icons")
 (load-library "dashboard")
+
+;; lsp elisp
+(load-library "lsp")
+
 
---
+++ modules/lsp/lsp.el
@@ -0,0 +1,17 @@
+;;=============== LSP MODE ==================;;
+;; The path to lsp-mode needs to be added to load-path as well as the
+;; path to the `clients' subdirectory.
+(add-to-list 'load-path (expand-file-name "lib/lsp-mode" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "lib/lsp-mode/clients" user-emacs-directory))
+
+;; Enable lsp.el
+(use-package lsp-mode
+  :straight t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  ;; if you want which-key integration
+  (lsp-enable-which-key-integration t))
+
+
