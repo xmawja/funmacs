96d508899413a281ca1f4bdde3397a301c87a349
Muja Mo <muja@foss.land>
2022-12-05 18:55:06 +0100

    FIX: vterm canot install
    vterm not showing when fresh config bootstrap installing.
    canot install vterm modules.

--- modules/vterm.el
+++ modules/vterm.el
@@ -4,19 +4,7 @@
   :straight t
   :init
   (setq vterm-toggle-fullscreen-p nil)
-  (add-to-list 'display-buffer-alist
-			   '((lambda (buffer-or-name _)
-                   (let ((buffer (get-buffer buffer-or-name)))
-                     (with-current-buffer buffer
-                       (or (equal major-mode 'vterm-mode)
-                           (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
-                (display-buffer-reuse-window display-buffer-at-bottom)
-                ;;(display-buffer-reuse-window display-buffer-in-direction)
-                ;;display-buffer-in-direction/direction/dedicated is added in emacs27
-                ;;(direction . bottom)
-                ;;(dedicated . t) ;dedicated is supported in emacs27
-                (reusable-frames . visible)
-                (window-height . 0.3))))
+  )
 
 ;; Enable vterm-toggle.el
 (use-package vterm-toggle
