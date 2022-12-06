3bcb0b27d37aeb482fb39f346a0b57f8632e2eba
Muja Mo <muja@foss.land>
2022-12-06 03:43:59 +0100

    ADEDD: icons to the widget headline
    To add icons to the widget headings and their items:
    using projecttile.el and all-the-icons.el

--- modules/dashboard.el
+++ modules/dashboard.el
@@ -7,6 +7,9 @@
   :init
   ;; show Dashboard in frames created with emacsclient -c
   (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
+  ;; add icons to the widget headings and their items
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
   )
 
 
