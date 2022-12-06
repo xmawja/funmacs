b76dacc28d977997b5916e21eec456a2e58d8906
Muja Mo <muja@foss.land>
2022-12-06 09:56:23 +0100

    FIX: rust lang related code

--- modules/lsp/lsp.el
+++ modules/lsp/lsp.el
@@ -44,19 +44,3 @@
 (use-package lsp-treemacs
   :after lsp)
 
-;; lsp language support
-;; rust
-(use-package rustic
-  :straight t
-  :bind (:map rustic-mode-map
-              ("M-j" . lsp-ui-imenu)
-              ("M-?" . lsp-find-references)
-              ("C-c C-c l" . flycheck-list-errors)
-              ("C-c C-c a" . lsp-execute-code-action)
-              ("C-c C-c r" . lsp-rename)
-              ("C-c C-c q" . lsp-workspace-restart)
-              ("C-c C-c Q" . lsp-workspace-shutdown)
-              ("C-c C-c s" . lsp-rust-analyzer-status))
-  :config
-  ;; comment to disable rustfmt on save
-  (setq rustic-format-on-save t))
