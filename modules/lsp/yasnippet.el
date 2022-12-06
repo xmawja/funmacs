684a4dae6e15cfd2ee2225df7335e698e38a0ce9
Muja Mo <muja@foss.land>
2022-12-06 05:24:18 +0100

    ENABLE: yasnippet lsp minor mode

--- modules/lsp/yasnippet.el
+++ modules/lsp/yasnippet.el
@@ -3,7 +3,10 @@
 (use-package yasnippet
   :straight t
   :config
-  (yas-global-mode 1)) ; activate yaggsnippet as global mode
+  (yas-global-mode 1) ; activate yaggsnippet as global mode
+  :hook
+  (lsp-mode . yas-minor-mode)
+  )
 ;; YASNIPPET-SNIPPETS
 (use-package yasnippet-snippets
   :straight t)
