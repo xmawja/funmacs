7cc5dbbe9366beb0248cdba4663a7a3cd2cb80cd
Muja Mo <muja@foss.land>
2022-12-06 10:04:20 +0100

    ADDED: golang format function

--- modules/lsp/lang/golang.el
+++ modules/lsp/lang/golang.el
@@ -2,11 +2,15 @@
 ;; Enable golang
 (use-package go-mode
   :straight t
-  :bind (:map go-mode-map
-			  ("C-," . 'akk/hydra-go/body))
   :hook
   (go-mode . lsp-deferred)
   :config
   ;; GOPATH to gopls
   (add-to-list 'exec-path "~/go/bin")
+  ;; Set up before-save hooks to format buffer and add/delete imports.
+  ;; Make sure you don't have other gofmt/goimports hooks enabled.
+  (defun lsp-go-install-save-hooks ()
+	(add-hook 'before-save-hook #'lsp-format-buffer t t)
+	(add-hook 'before-save-hook #'lsp-organize-imports t t))
+  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
   )
