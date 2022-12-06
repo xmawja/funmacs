df677e925f95c2993b680664501eb189dca5a00f
Muja Mo <muja@foss.land>
2022-12-06 06:55:11 +0100

    ADDED: Rust language development environment support

--- modules/lsp/lsp.el
+++ modules/lsp/lsp.el
@@ -33,6 +33,7 @@
   (c++-mode . lsp-deferred)           ; ENABLE C++ PROGRAMING
   (c-or-c++-mode . lsp-deferred)      ; ENABLE C/C++ PROGRAMING
   (sh-mode . lsp-deferred)            ; ENABLE SHELL SCRIPT
+  (rustic-mode . lsp-deferred)        ; ENABLE RUST PROGRAMING
   )
 
 ;; Optionaly
@@ -42,3 +43,21 @@
 ;; lsp-treemacs integration
 (use-package lsp-treemacs
   :after lsp)
+
+
+;; lsp language support
+;; rust
+(use-package rustic
+  :straight t
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t))
