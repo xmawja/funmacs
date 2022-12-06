78056428c83a91c79ec788ca3891ff71365eb646
Muja Mo <muja@foss.land>
2022-12-06 03:42:33 +0100

    ENABLE: dashboard to show in emacs daemon

--- modules/dashboard.el
+++ modules/dashboard.el
@@ -3,6 +3,10 @@
 (use-package dashboard
   :straight t
   :config
-  (dashboard-setup-startup-hook))
+  (dashboard-setup-startup-hook)
+  :init
+  ;; show Dashboard in frames created with emacsclient -c
+  (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
+  )
 
 
