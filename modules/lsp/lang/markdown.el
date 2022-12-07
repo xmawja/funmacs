3f7473da55514a9e4cf1373ab4ac452044e06aa6
Muja Siyam <contact@muja.dev>
2022-12-08 00:13:51 +0100

    FIX: mardown-mode.el to accept all .md && .markdown extension

--- modules/lsp/lang/markdown.el
+++ modules/lsp/lang/markdown.el
@@ -2,7 +2,8 @@
 ;; Enable markdown-mode.el non lsp
 (use-package markdown-mode
   :straight t
-  :mode
-  ("README\\.md\\'" . gfm-mode)
-  :init
-  (setq markdown-command "multimarkdown"))
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
