fc7be03bec6dde27ed2f61499fa49587148115c9
Muja Siyam <contact@muja.dev>
2022-12-09 19:29:49 +0100

    ADDED: css-ls.el && emmet-ls support lsp
    css-ls to enable support for CSS/LessCSS/SASS/SCSS file extension
    emmet-ls to enable snippet for we-mode lsp

--- modules/lsp/lang/web.el
+++ modules/lsp/lang/web.el
@@ -1,7 +1,15 @@
 ;;================== WEB-MODE ====================;;
 ;; Enable web-mode.el
 (use-package web-mode
-  :mode "\\.html?\\'"
+  :mode
+  "\\.html?\\'"
+  "\\.css?\\'"
   :hook ((web-mode . (lambda ()
-                       (when (require 'lsp-html nil t)
-                         (lsp))))))
+                       (when
+						 ;; auto emerge emmet-ls
+						 (require 'emmet-ls nil t)
+						 ;; auto emerge lsp-html
+						 (require 'lsp-html nil t)
+						 ;; auto emerge css-ls
+						 (require 'css-ls nil t)
+                         (lsp-deferred))))))
