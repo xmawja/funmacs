35fb2a65d02f2d8003e7df1bf5c0f79ecf05250e
Muja Siyam <contact@muja.dev>
2022-12-18 19:41:25 +0100

    ADDED: emacs.el

---
+++ core/emacs.el
@@ -0,0 +1,30 @@
+;;; emacs.el -*- lexical-binding: t; -*-
+;; This file has been generated from init.el file. DO NOT EDIT.
+;; Sources are available from https://github.com/xmawja/funmacs
+
+;; Copyright (C) 2022 Muja Siyam
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+;; Enable emacs.el
+(use-package emacs
+  :init
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-italic-constructs t
+        modus-themes-bold-constructs nil
+        modus-themes-region '(bg-only no-extend))
+  :config
+  ;; Load the theme of your choice:
+  (load-theme 'modus-operandi) ;; OR (load-theme 'modus-vivendi)
+  :bind ("<f5>" . modus-themes-toggle))
