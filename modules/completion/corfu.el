b409cea86eb06895b22a68fe336a42fd6c549a92
Muja Siyam <contact@muja.dev>
2022-12-18 22:09:26 +0100

    ENABLE: corfu.el orderless field separator.

--- modules/completion/corfu.el
+++ modules/completion/corfu.el
@@ -23,7 +23,7 @@
   :custom
   (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
   (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  (corfu-separator ?\s)          ;; Orderless field separator
   ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
   ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
   ;; (corfu-preview-current nil)    ;; Disable current candidate preview
