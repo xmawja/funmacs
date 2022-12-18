3a610c2d7555ae6233c0ac68677cb1b0dcfd4b10
Muja Siyam <contact@muja.dev>
2022-12-18 17:08:10 +0100

    FIX: init.el structure

--- init.el
+++ init.el
@@ -2,11 +2,10 @@
 ;; import directorys
 (add-to-list 'load-path "~/.emacs.d/core")
 (add-to-list 'load-path "~/.emacs.d/modules")
+(add-to-list 'load-path "~/.emacs.d/modules/org")
 (add-to-list 'load-path "~/.emacs.d/modules/ui")
 (add-to-list 'load-path "~/.emacs.d/modules/lsp")
 (add-to-list 'load-path "~/.emacs.d/modules/lsp/lang")
-(add-to-list 'load-path "~/.emacs.d/modules/org")
-(add-to-list 'load-path "~/.emacs.d/modules/eglot")
 (add-to-list 'load-path "~/.emacs.d/modules/completion")
 
 ;; core elisp 
@@ -18,12 +17,8 @@
 (load-library "org")
 (load-library "vertico")
 (load-library "magit")
-(load-library "themes")
 (load-library "treemacs")
-(load-library "icons")
-(load-library "dashboard")
 (load-library "vterm")
-(load-library "modeline")
 (load-library "projectile")
 (load-library "undo-tree")
 (load-library "company")
@@ -31,14 +26,18 @@
 (load-library "flycheck")
 (load-library "benchmark-init")
 
-;; modules-eglot elisp
-(load-library "eglot")
+;;modules-ui elisp
+(load-library "themes")
 (load-library "all-the-icons")
+(load-library "dashboard")
+(load-library "modeline")
+
 ;; modules-completion elisp
 (load-library "corfu")
 (load-library "cape")
 
 ;; modules-lsp elisp
+(load-library "eglot")
 (load-library "lsp")
 (load-library "dap")
 
