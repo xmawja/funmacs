e2f1881f9908ae4b7420f125c3a0a31549bce894
Muja Siyam <contact@muja.dev>
2022-12-18 17:15:05 +0100

    FIX: corfu.el

--- modules/completion/corfu.el
+++ modules/completion/corfu.el
@@ -2,9 +2,9 @@
 ;; Enable corfu.el
 (use-package corfu
   ;; Optional customizations
-   :custom
-  ;;(corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;;(corfu-auto t)                 ;; Enable auto completion
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
   ;; (corfu-separator ?\s)          ;; Orderless field separator
   ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
   ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
