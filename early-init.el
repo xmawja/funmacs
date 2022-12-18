9bd944d813af1c947fb52766badeb3f959de59d1
Muja Siyam <contact@muja.dev>
2022-12-18 19:39:05 +0100

    ADDED: early-init.el file.
    49.4.6 The Early Init File
    Most customizations for Emacs should be put in the normal init file. See The Emacs Initialization File. However, it is sometimes necessary to have customizations take effect during Emacs startup earlier than the normal init file is processed. Such customizations can be put in the early init file, ~/.config/emacs/early-init.el or ~/.emacs.d/early-init.el. This file is loaded before the package system and GUI is initialized, so in it you can customize variables that affect the package initialization process, such as package-enable-at-startup, package-load-list, and package-user-dir. Note that variables like package-archives which only affect the installation of new packages, and not the process of making already-installed packages available, may be customized in the regular init file. See Package Installation.
    
    We do not recommend that you move into early-init.el customizations that can be left in the normal init files. That is because the early init file is read before the GUI is initialized, so customizations related to GUI features will not work reliably in early-init.el. By contrast, the normal init files are read after the GUI is initialized. If you must have customizations in the early init file that rely on GUI features, make them run off hooks provided by the Emacs startup, such as window-setup-hook or tty-setup-hook. See Hooks.
    
    For more information on the early init file, see Init File in The
    Emacs Lisp Reference Manual.
    
    https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html

---
+++ early-init.el
@@ -0,0 +1,65 @@
+;;; early-init.el -*- lexical-binding: t; -*-
+;; This file has been generated from init.el file. DO NOT EDIT.
+;; Sources are available from https://github.com/xmawja/funmacs
+
+;; Copyright (C) 2022 Muja Siyam
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Provides early initialization for Emacs 27.1+ Do not initialize the
+;; package manager.  This is done in `init.el'.  The optimization of
+;; the early init comes from both Doom Emacs' config as well as Prot's
+;; config.
+;; See https://github.com/hlissner/doom-emacs/blob/develop/early-init.el
+;; See https://protesilaos.com/dotemacs/
+
+
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; In Emacs 27+, package initialization occurs before `user-init-file' is
+;; loaded, but after `early-init-file'. We want to keep from loading at startup.
+;; disable package.el load at startup if you use somthing like straight.el. 
+(setq package-enable-at-startup nil)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(mode-line-format . 0) default-frame-alist)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+
+;; Create a package-quickstart.el
+(setq package-quickstart t)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+;; Ignore X resources; its settings would be redundant with the other settings
+;; in this file and can conflict with later config (particularly where the
+;; cursor color is concerned).
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+;;(setq native-comp-async-report-warnings-errors nil) 	; Stop showing compilation warnings on startup
+
+;; Make the initial buffer load faster by setting its mode to fundamental-mode
+(customize-set-variable 'initial-major-mode 'fundamental-mode)
+
