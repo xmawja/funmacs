6e30c67eb7d983ee262f230079fa2d8f9771a52f
Muja Siyam <contact@muja.dev>
2022-12-19 10:25:24 +0100

    ADDED: monokai-pro-theme.el && FIX: treemacs.el icons color.

--- modules/ui/themes.el
+++ modules/ui/themes.el
@@ -31,9 +31,15 @@
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   ;;(doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-colors") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
+;; Enable monokai-pro-theme.el
+;;(use-package monokai-pro-theme
+;;  :straight t
+;;  :config
+;;  (load-theme 'monokai-pro t)
+;;  )
 
