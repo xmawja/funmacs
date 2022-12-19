c7aca7d823dab48033657b34ee16ad05af3dbcd1
Muja Siyam <contact@muja.dev>
2022-12-19 18:43:01 +0100

    ENABLE: go-mode.el to be sued by eglot.el.

--- modules/lsp/lang/golang.el
+++ modules/lsp/lang/golang.el
@@ -21,17 +21,21 @@
 (use-package go-mode
   :straight t
   :hook
-  ;; set lsp-mode 
-  (go-mode . lsp-deferred)
-  ;; set company as default code compilaton
-  (go-mode . company-mode)
+  ;; uncomment and choose your prefared lsp packend server.
+  ;; lsp-mode.
+  ;;(go-mode . lsp-deferred)
+  ;; eglot mode.
+  (go-mode . eglot-ensure)
+  ;; company-mode work best with lsp-mode if use eglot consider corfo.el.
+  ;; company code compilaton
+  ;;(go-mode . company-mode)
   :config
   ;; GOPATH to gopls
   (add-to-list 'exec-path "~/go/bin")
   ;; Set up before-save hooks to format buffer and add/delete imports.
   ;; Make sure you don't have other gofmt/goimports hooks enabled.
-  (defun lsp-go-install-save-hooks ()
-	(add-hook 'before-save-hook #'lsp-format-buffer t t)
-	(add-hook 'before-save-hook #'lsp-organize-imports t t))
-  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+  ;; (defun lsp-go-install-save-hooks ()
+  ;; 	(add-hook 'before-save-hook #'lsp-format-buffer t t)
+  ;; 	(add-hook 'before-save-hook #'lsp-organize-imports t t))
+  ;; (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
   )
