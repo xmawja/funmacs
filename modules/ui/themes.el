227f1bbc674d3a401bcc6862dd1569eb75971b1d
Muja Siyam <contact@muja.dev>
2023-09-23 17:54:35 +0000

    CHANGED: defautl theme

--- modules/ui/themes.el
+++ modules/ui/themes.el
@@ -24,7 +24,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+ (load-theme 'doom-monokai-pro t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
