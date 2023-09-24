47a6c5c7b8e74e1637afa1d8a754471175c7c1f3
Muja Siyam <contact@muja.dev>
2023-09-24 05:00:21 +0000

    ADDED: treesit-auto Automatic installation, usage, and fallback
    Automatically install and use tree-sitter major modes in Emacs 29+. If
    the tree-sitter version can’t be used, fall back to the original major
    mode.
    https://github.com/renzmann/treesit-auto.git

---
+++ modules/lsp/treesitter/treesit-auto.el
@@ -0,0 +1,27 @@
+;;; typescript-conf.el -*- lexical-binding: t; -*-
+;; This file has been generated from init.el file. DO NOT EDIT.
+;; Sources are available from https://github.com/xmawja/funmacs
+
+;; Copyright (C) 2022 - 2023 Muja Siyam
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+
+;; load library 'PATH'
+(use-package treesit-auto
+  :config
+  (global-treesit-auto-mode)
+  ) ;; End
+
+;; end 'treesit-auto' file.
