13574359449a26f7421821e07bcdcd9c2063e277
Muja Siyam <contact@muja.dev>
2023-09-24 07:22:44 +0000

    ADDED: markdown-soma Live Markdown preview for Emacs
    markdown-soma is an Emacs minor-mode which gives you live rendering of Markdown to HTML.
    URL: https://github.com/jasonm23/markdown-soma.git

--- modules/lsp/lang/markdown/markdown-soma.el
+++ modules/lsp/lang/markdown/markdown-soma.el
@@ -1,5 +1,5 @@
-;;; markdown-conf.el -*- lexical-binding: t; -*-
-;; This file has been generated from init.el file. DO NOT EDIT.
+;;; markdown.el -*- lexical-binding: t; -*-
+;; This file has been generated from funmacs.org file. DO NOT EDIT.
 ;; Sources are available from https://github.com/xmawja/funmacs
 
 ;; Copyright (C) 2022 - 2023 Muja Siyam
@@ -17,8 +17,21 @@
 ;; For a full copy of the GNU General Public License
 ;; see <https://www.gnu.org/licenses/>.
 
-;; load library 'PATH'
-(load-library "markdown")
-(load-library "markdown-soma")
+;; Enable markdown-mode.el
+;; Or using hooks
+(use-package markdown-soma
+  :straight t
+  :hook ((markdown-mode org-mode) . markdown-soma-mode)
+  :init
+  ;; default render buffer hooks
+  (setq markdown-soma--render-buffer-hooks
+        '(after-revert-hook
+          after-save-hook
+          after-change-functions
+          post-command-hook))
+  :config
+  ;; rust cargo bin PATH.
+  (add-to-list 'exec-path "~/.cargo/bin")
+  ) ;; End
 
-;; end 'markdown-conf' file.
+;; end 'markdown-mode' file.
