4a5dd36fbc216f4777d5fc6a3190087f64408b0a
Muja Siyam <contact@muja.dev>
2023-09-26 13:07:48 +0000

    FIX: backup.el match directoy

--- core/backup.el
+++ core/backup.el
@@ -32,6 +32,21 @@
   (version-control t "use versioned backups")
   :config
   (setq confirm-kill-processes nil)
+  
+  (setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+
+  
+  (setq make-backup-files nil)
+
+  
+;; auto-save-mode doesn't create the path automatically!
+(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
+
+(setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
+      auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
+
+
+
   ) ;; end backup.el
 
 ;; ;; backup-dir.el package.
