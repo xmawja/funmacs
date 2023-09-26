9fa0a6f2701b2a4733f7906663aa85258f59742b
Muja Siyam <contact@muja.dev>
2023-09-26 13:05:44 +0000

    ENABLED: emacs.el tab indent

--- core/emacs.el
+++ core/emacs.el
@@ -55,6 +55,20 @@
   ;; ;; Load the theme of your choice:
   ;; (load-theme 'modus-operandi) ;; OR (load-theme 'modus-vivendi)
   ;; :bind ("<f5>" . modus-themes-toggle)
+
+  ;; 'corfu.el' configuration
+  ;; TAB cycle if there are only few candidates
+  (setq completion-cycle-threshold 3)
+
+  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (setq tab-always-indent 'complete)
+  
   ) ;; end emacs.el
 
 ;; end 'emacs' file.
