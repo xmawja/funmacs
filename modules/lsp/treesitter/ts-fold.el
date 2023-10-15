ad4326d30f261a8b2df73f7acd3f4d1bbcafea30
Muja Siyam <contact@muja.dev>
2023-10-15 17:33:45 +0200

     : ts-fold indicators

--- modules/lsp/treesitter/ts-fold.el
+++ modules/lsp/treesitter/ts-fold.el
@@ -23,10 +23,37 @@
 ;; ts-fold builds on top of elisp-tree-sitter
 ;; to provide code folding based on the tree-sitter syntax tree.
 (use-package ts-fold
-  :straight (ts-fold :type git :host github :repo "emacs-tree-sitter/ts-fold")
+  :straight (ts-fold
+             :type git
+             :host github
+             :repo "emacs-tree-sitter/ts-fold")
   :config
   (global-ts-fold-mode)
-  (global-ts-fold-indicators-mode)
+  (global-ts-fold-indicators-mode 1)
+
+  (setq ts-fold-indicators-fringe 'right-fringe)
+  (setq ts-fold-indicators-priority 30)
+  (setq ts-fold-indicators-face-function
+        (lambda (pos &rest _)
+          ;; Return the face of it's function.
+          (line-reminder--get-face (line-number-at-pos pos t))))
+
+  (advice-add 'line-reminder-transfer-to-saved-lines
+              :after
+              ;; Refresh indicators for package `ts-fold'.
+              #'ts-fold-indicators-refresh)
+  
+  ;; Example of ts-fold-range-alist's structure
+  '((c-mode     . c-folding-definitions) ;; <language>-folding-definitions is structured as shown below
+    (css-mode   . css-folding-definitions)
+    (go-mode    . go-folding-definitions)
+    (scala-mode . scala-folding-definitions)
+    ...)
+
+  ;; Examle of a folding definition alist
+  (setq css-folding-definitions
+        (block   . ts-fold-range-seq)
+        (comment . ts-fold-range-c-like-comment))
   :bind
   (:map global-map
         ("C-c f O" . ts-fold-open-all)
