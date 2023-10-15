db5cb655e18899286552909c72ef701ca78248a7
Muja Siyam <contact@muja.dev>
2023-10-15 23:35:48 +0200

     nerd-icons fonts

--- modules/ui/nerd-icons.el
+++ modules/ui/nerd-icons.el
@@ -29,7 +29,7 @@
   ;; The Nerd Font you want to use in GUI
   ;; "Symbols Nerd Font Mono" is the default and is recommended
   ;; but you can use any other Nerd Font if you want
-  (nerd-icons-font-family "Symbols Nerd Font Mono")
+  (nerd-icons-font-family "JetBrainsMono Nerd Font Mono")
   )
 
 (use-package nerd-icons-dired
